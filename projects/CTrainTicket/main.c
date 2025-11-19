1092567523ba9239dd051da95c17e94264d64987
NONGFUER <13852291705@163.com>
2025-11-19 09:11:56 +0800

    火车订票系统程序入口

---
+++ projects/CTrainTicket/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "menu.h"
+#include "auth.h"
+#include "control.h"
+
+
+
+int main()
+{
+    menu_choice();//主菜单选择功能
+    return 0;
+}
