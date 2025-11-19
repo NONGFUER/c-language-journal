a8abad1c2c0066c5499e0b49506c5acab78f202f
NONGFUER <13852291705@163.com>
2025-11-19 09:13:32 +0800

    菜单打印相关

---
+++ projects/CTrainTicket/menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+void menu();
+void menu_head_style();//头部菜单样式
+void menu_end_style();//尾部菜单样式
+
+void menu();//程序主界面入口菜单
+
+void menu_user();//旅客功能菜单
+
+void menu_ticket_query();//查询火车车票信息子菜单
+
+void menu_admin();//管理员功能菜单
+void menu_admin_user();//管理旅客用户信息-子菜单
+void menu_train();//管理车次信息-子菜单
+
+
+#endif
