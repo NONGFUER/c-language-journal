522224f52bd4903b679bf4d1e38ac0aa13d9ea76
NONGFUER <13852291705@163.com>
2025-11-20 09:04:12 +0800

    增加加载用户信息

--- projects/CTrainTicket/main.c
+++ projects/CTrainTicket/main.c
@@ -3,12 +3,22 @@
 #include <stdlib.h>
 #include "menu.h"
 #include "auth.h"
+#include "user.h"
 #include "control.h"
+#include "types.h"
+
+// 定义全局变量all_users和adm
+user all_users[10];
+int user_count = 0;
+
+admin adm = { "admin", "123456" };
 
 
 
 int main()
 {
+    user_count = load_user_info(all_users);//加载用户信息
+    //clear_file("user_info.txt");//手动清空
     menu_choice();//主菜单选择功能
     return 0;
 }
